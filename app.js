1d891a08dfdf84fc8392f5ec179b72252eb82afb
Dhruv Popuri <dhruvxdpotter@gmail.com>
2022-05-31 17:58:25 +0530

    Initialised project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('',(req,res)=>{
+    res.send('Web server initialised!')
+})
+
+app.listen(3000, ()=>{
+    console.log('server is up and running on port 3000')
+})
