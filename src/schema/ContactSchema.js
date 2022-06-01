f51dd5e7c1efa44945ba82a6ca2f187b6e3e40fe
Dhruv Popuri <dhruvxdpotter@gmail.com>
2022-06-01 08:44:09 +0530

    created contact schema and model

---
+++ src/schema/ContactSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const User = require('./UserSchema')
+
+const contactSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    address:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    mobile:{
+        type: Number,
+        minLength:10,
+        maxLength:12 //In case people give with country code!
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+})
+
+const Contact = mongoose.model('Contact',contactSchema)
+
+module.exports = Contact
